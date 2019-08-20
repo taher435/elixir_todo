1dc259eb976e805716f3b555c21754580acfa520
Taher Dhilawala <taher435@gmail.com>
2019-08-20 23:58:12 +0530

    Remove load_from_sys key

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_todo, ElixirTodoWeb.Endpoint,
-  load_from_system_env: true,
   url: [scheme: "https", host: "elixir-todo-pretest.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
