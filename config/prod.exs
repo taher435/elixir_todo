ae589bac7efd9d5b409eeaa1c9ef905aa8f014c6
Taher Dhilawala <taher435@gmail.com>
2019-08-21 15:48:41 +0530

    A new app name

--- config/prod.exs
+++ config/prod.exs
@@ -12,14 +12,13 @@ use Mix.Config
 config :elixir_todo, ElixirTodoWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   load_from_system_env: true,
-  url: [scheme: "https", host: "elixir-todo-pretest.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "deploy-test-ltts.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
-
 
 config :elixir_todo, ElixirTodo.Repo,
   adapter: Ecto.Adapters.Postgres,
